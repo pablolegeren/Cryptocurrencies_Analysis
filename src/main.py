83cd7d38502b55f86d1e5a4f6d10f06dfd04384f
Iker Yañez <ikeryanez@gmail.com>
2023-11-25 00:06:12 +0100

    Menu de selección de par y descarga de datos

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+import descargaDatos
 def main():
-    print("Prueba Integración Continua")
+    descargaDatos.menu_seleccion_par()
 
 main()
