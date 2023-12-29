de82ce15da91a678743d24228d815358976f2264
Iker Yañez <ikeryanez@gmail.com>
2023-12-29 11:05:27 +0100

    Añadido gitignore

--- src/DescargadorDatos.py
+++ src/DescargadorDatos.py
@@ -6,7 +6,6 @@ class DescargadorDatos:
     def __init__(self):
         self.api = krakenex.API()
         self.kraken_api = KrakenAPI(self.api)
-
     def descargar_datos(self, par, intervalo=10080): #Semanal, 10080 minutos en una semana; diario 1440
         ohlc, last = self.kraken_api.get_ohlc_data(par,intervalo,ascending = True)
         return ohlc
